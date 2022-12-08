5d46369d6b661191a2c8af3dfd3643e360c9d7f2
Anna Sintsova <ansintsova@ethz.ch>
2022-12-08 11:03:04 +0100

    Finished basic suite of tests for input functions. Added pytest to requirements

--- test/test_input.py
+++ test/test_input.py
@@ -58,8 +58,21 @@ def test_parse_uncalibrated(cutoffs):
     expected_cutoffs, _, _ = cutoffs
     assert output_cutoffs == expected_cutoffs
 
-# todo same for  verybesthit and allhits (parametrize)
+def test_parse_verybesthit(cutoffs):
+    args = FakeArgs(params= {'mode': 'extraction', 'b': 'fetchmgs/data/MG_BitScoreCutoffs.verybesthit.txt',
+                             'v': True})
+    output_cutoffs = parse_cutoffs(args)
+    uncalibrated, allhits, besthits = cutoffs
+    assert output_cutoffs == besthits
 
+def test_parse_allhits(cutoffs):
+    args = FakeArgs(params= {'mode': 'extraction', 'b': 'fetchmgs/data/MG_BitScoreCutoffs.allhits.txt',
+                             'v': False})
+    output_cutoffs = parse_cutoffs(args)
+    uncalibrated, allhits, besthits = cutoffs
+    assert output_cutoffs == allhits
+
+# Test import_files
 def test_import_files_defaults_calibration(cutoffs, hmms):
     args = FakeArgs({'l': 'fetchmgs/data',
                      'c': 'all',
@@ -73,4 +86,35 @@ def test_import_files_defaults_calibration(cutoffs, hmms):
     assert out_cutoffs == expected_cutoffs
     assert out_hmms == all_hmms
 
-# todo same with lots of different params
+def test_import_files_defaults_extraction_verybest(cutoffs, hmms):
+    args = FakeArgs({'l': 'fetchmgs/data',
+                     'c': 'all',
+                     'mode': 'extraction',
+                     'v': True,
+                     'file': TESTDIR/'example_data.faa',
+                     'map':  'test/known_positives.map',
+                      })
+    out_hmms, out_cutoffs, valid_map, prot_records, nucl_records = import_files(args)
+    uncalibrated, allhits, besthits = cutoffs
+    assert out_cutoffs == besthits
+    assert out_hmms == hmms
+
+def test_import_files_defaults_extraction_allhits(cutoffs, hmms, first_sequence):
+    args = FakeArgs({'l': 'fetchmgs/data',
+                     'c': 'all',
+                     'mode': 'extraction',
+                     'v': False,
+                     'file': TESTDIR/'example_data.faa',
+                     'map':  'test/known_positives.map',
+                      })
+    out_hmms, out_cutoffs, valid_map, prot_records, nucl_records = import_files(args)
+    uncalibrated, allhits, besthits = cutoffs
+    fseq = next(prot_records)
+    seq_id, nt_seq, aa_seq = first_sequence
+    assert out_cutoffs == allhits
+    assert out_hmms == hmms
+    assert fseq.id == seq_id
+    assert fseq.seq == aa_seq
+
+# todo test c != 'all'
+# todo check seq in all import_files tests
