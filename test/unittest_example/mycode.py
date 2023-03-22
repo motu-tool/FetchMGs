bcb786d3a2b769a8f17e8de4fb18e8cc7887179b
Samuel Miravet-Verde <samuelmiver@gmail.com>
2023-03-22 09:42:05 +0100

    Legacy test hmms (#13)
    * first working version of basic operation
    
    * fully reproduce extraction mode on example data
    
    * calibration mode works
    
    * calibration mode works
    
    * added hmmResults output
    
    * package restructure
    
    * pre-review commit
    
    * fully functionalised
    
    * move fetchmgs to pip structure
    
    * Added pytest minimal example
    
    * hmm extraction tests
    
    * clean test_hmms branch
    
    ---------
    
    Co-authored-by: cmfield <fieldc@ethz.ch>
    Co-authored-by: Hans <hansr@ethz.ch>
    Co-authored-by: Chris Field <36225262+cmfield@users.noreply.github.com>

---
+++ test/unittest_example/mycode.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def multiply(a,b):
+    """
+    Given two integers a and b,
+    Returns the product of a and b
+
+    Example:
+    multiply(3,5) = 15
+    """
+    return a*b
+
+if __name__ == '__main__':
+    # Multiply function
+    print(multiply(3, 5))
