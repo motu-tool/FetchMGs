ad59f0a7498e850b7ca2fe0fc28174855446a380
samseagreen <samuelmiver@gmail.com>
2023-03-22 17:26:21 +0100

    cli tests

--- test/test_cli.py
+++ test/test_cli.py
@@ -11,16 +11,43 @@ TEST_DATA_DIR  = f'{TEST_DIR}/test_data/'
 sys.path.insert(0, PARENT_DIR) 
 from fetchMGs import cli
 
-
 def test_cli_no_mode():
-    """ Test for the command line interface output when no mode is defined """
+    """
+    Test for the command line interface output when no mode is defined 
+    
+    If cli arguments are change, regenerate the cli txt files in test data by 
+    python3 fetchMGs.py -h > ../test/test_data/cli_no_mode.txt
+    """
 
     proc = subprocess.Popen(["python", f"{PARENT_DIR}/fetchMGs.py", "-h"], stdout=subprocess.PIPE)
-    test_out = proc.communicate()[0].decode()
-    print(test_out)
+    test_out = proc.communicate()[0].decode().split()
+    original_out = open(f"{TEST_DATA_DIR}/cli_no_mode.txt", "r").read().split()
+    assert test_out == original_out
 
-    original_out = open(f"{TEST_DATA_DIR}/cli_no_mode.txt", "r")
-    print(original_out.read())
 
-    print(test_out == original_out)
-test_cli_no_mode()
+def test_cli_calibration():
+    """
+    Test for the command line interface output when calibration mode is selected.
+    
+    If cli arguments are change, regenerate the cli txt files in test data by 
+    python3 fetchMGs.py -m calibration -h > ../test/test_data/cli_calibration.txt
+    """
+
+    proc = subprocess.Popen(["python", f"{PARENT_DIR}/fetchMGs.py", "-m", "calibration", "-h"], stdout=subprocess.PIPE)
+    test_out = proc.communicate()[0].decode().split()
+    original_out = open(f"{TEST_DATA_DIR}/cli_calibration.txt", "r").read().split()
+    assert test_out == original_out
+
+
+def test_cli_extraction():
+    """
+    Test for the command line interface output when extraction mode is selected.
+    
+    If cli arguments are change, regenerate the cli txt files in test data by 
+    python3 fetchMGs.py -m extraction -h > ../test/test_data/cli_extraction.txt
+    """
+
+    proc = subprocess.Popen(["python", f"{PARENT_DIR}/fetchMGs.py", "-m", "extraction", "-h"], stdout=subprocess.PIPE)
+    test_out = proc.communicate()[0].decode().split()
+    original_out = open(f"{TEST_DATA_DIR}/cli_extraction.txt", "r").read().split()
+    assert test_out == original_out
